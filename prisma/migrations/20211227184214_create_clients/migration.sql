27a05905d6688f7482cb31c0f02892af04dee6ba
Marcos Corsi <marcosvcorsi@gmail.com>
2021-12-27 15:42:29 -0300

    feat: create clients model and migration

---
+++ prisma/migrations/20211227184214_create_clients/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "clients" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "clients_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "clients_username_key" ON "clients"("username");
