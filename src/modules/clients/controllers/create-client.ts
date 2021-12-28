4ad47688b0a16129797b653813c60cfedf37088b
Marcos Corsi <marcosvcorsi@gmail.com>
2021-12-28 01:08:35 -0300

    refactor: add controller and use case interfaces

--- src/modules/clients/controllers/create-client.ts
+++ src/modules/clients/controllers/create-client.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from "express";
+import { Controller } from "../../../shared/controllers";
 import { BcryptHashProvider } from "../../../shared/providers/bcrypt";
 import { PrismaClientRepository } from "../repositories/prisma/client";
 import { CreateClientUseCase } from "../use-cases/create-client";
 
-export class CreateClientController {
+export class CreateClientController implements Controller {
   async handle(request: Request, response: Response): Promise<Response> {
     const { username, password } = request.body;
 
--- src/modules/clients/use-cases/create-client.ts
+++ src/modules/clients/use-cases/create-client.ts
@@ -1,4 +1,5 @@
 import { HashProvider } from "../../../shared/providers/hash";
+import { UseCase } from "../../../shared/use-cases";
 import { Client } from "../entities/client";
 import { ClientAlreadyExistsError } from "../errors/client-already-exists";
 import { ClientRepository } from "../repositories/client";
@@ -8,15 +9,13 @@ type Params = {
   password: string;
 };
 
-type Result = Promise<Client>;
-
-export class CreateClientUseCase {
+export class CreateClientUseCase implements UseCase<Params, Client> {
   constructor(
     private readonly clientRepository: ClientRepository,
     private readonly hashProvider: HashProvider
   ) {}
 
-  async execute({ username, password }: Params): Result {
+  async execute({ username, password }: Params): Promise<Client> {
     const existingUsername = await this.clientRepository.findByUsername(
       username
     );
